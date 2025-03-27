c4d57141fa1524ceeadce21c5270b98464d99d25
ragul <ragulraja2808@gmail.com>
2025-03-27 21:42:26 +0530

    Add LoadingScreen component for loading state indication

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,5 @@
+export const LoadingScreen = () => {
+    return <div className="fixed inset-0 z-50 bg-black text-gray-100 items-center justify-center">
+        <div className="mb-4 text-4xl font-mono font-bold">Hello world <span className="animate-blink ml-1"> |</span></div>
+    </div>
+}
