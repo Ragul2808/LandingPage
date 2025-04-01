4cbe4709083eb7d037502bed2f0b57af5d9f2804
ragul <ragulraja2808@gmail.com>
2025-04-01 22:23:08 +0530

    Add loading state management and integrate LoadingScreen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { LoadingScreen } from "./components/LoadingScreen"
+import { useState } from "react"
+import { Navbar } from "./components/Navbar"
 
 
 function App() {
+  const [isloading, setISLoading] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+     {!isloading &&<LoadingScreen  onComplete={()=> setISLoading(true)}/>}
+     <div className="min-h-screen transition-opacity duration-700 ${
+      isloading ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100">
+        <Navbar menuOpen={menuOpen
+          } setMenuOpen={setMenuOpen}/>
+      
+      </div>
     </>
   )
 }
